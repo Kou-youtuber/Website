c634ebe1a82d1a3c312bbc6e529db1293824706e
Kou-youtuber <uehara.ko.1016@gmail.com>
2026-07-05 22:40:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,3 +112,28 @@ topButton.addEventListener("click",()=>{
     });
 
 });
+// =====================
+// スライダー
+// =====================
+
+const slides=document.querySelectorAll(".slide");
+
+let currentSlide=0;
+
+function changeSlide(){
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide++;
+
+if(currentSlide>=slides.length){
+
+currentSlide=0;
+
+}
+
+slides[currentSlide].classList.add("active");
+
+}
+
+setInterval(changeSlide,4000);
